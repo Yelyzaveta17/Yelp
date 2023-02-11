0939710b9e5e9a7edef2fae801238a4a9e0a203d
Yelyzaveta17 <liza.shkuropat@outlook.com>
2023-02-10 18:14:19 -0800

    final test

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
-
     useUnifiedTopology: true,
     useFindAndModify: false
 });
